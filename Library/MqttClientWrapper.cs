640d3c58b0ba33c2ff11cbae2305c8b61a88a21f
Szewczuk937 <dmytrofjb@gmail.com>
2023-09-18 10:17:46 +0200

    s

---
+++ Library/MqttClientWrapper.cs
@@ -0,0 +1,53 @@
+﻿using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class MqttClientWrapper
+    {
+        private MqttClient mqttClient;
+
+        public MqttClientWrapper(string brokerAddress)
+        {
+            mqttClient = new MqttClient(brokerAddress);
+            mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+        }
+
+        public event Action<string, string> MessageReceived;
+
+        public void Connect()
+        {
+            mqttClient.Connect(Guid.NewGuid().ToString());
+        }
+
+        public void Disconnect()
+        {
+            mqttClient.Disconnect();
+        }
+
+        public void Publish(MqttMessage mqttMessage)
+        {
+            if (mqttClient.IsConnected)
+            {
+                mqttClient.Publish(mqttMessage.Topic, Encoding.UTF8.GetBytes(mqttMessage.Message), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, false);
+            }
+            else
+            { 
+                Console.WriteLine("Błąd: Klient MQTT nie jest połączony z brokerem MQTT.");
+            }
+        }
+
+        private void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string topic = e.Topic;
+            string message = Encoding.UTF8.GetString(e.Message);
+            MessageReceived?.Invoke(topic, message);
+        }
+    }
+
+}
---
+++ Library/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string brokerAddress = "broker.hivemq.com";
+            MqttClientWrapper mqttClient = new MqttClientWrapper(brokerAddress);
+            mqttClient.Connect();
+
+            MqttMessage message = new MqttMessage
+            {
+                Topic = "topic",
+                Message = "Hello, MQTT! \n My nawe Dmytro Shevchuk"
+            };
+
+            mqttClient.Publish(message);
+
+            mqttClient.Disconnect();
+        }
+    }
+}
